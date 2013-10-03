7be952379a5e9b4e225fb7a0e3c17bae071ff6f6
SeanJA <sean.sandy@gmail.com>
2013-10-03 18:36:11 -0400

    Deleted the contents of platforms

--- platforms/android/assets/www/js/config.sample.js
+++
@@ -1,6 +0,0 @@
-var config = {
-	key: '293879dsfvb90a9sd',
-	// postTo: 'http://192.168.1.100/free-sofa/server/got.php',
-	postTo: 'http://192.168.1.100/free-sofa/server/got.php',
-	getFrom: 'http://192.168.1.100/free-sofa/server/get.php'
-}
--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-c:\xampp\htdocs\free-sofa\platforms\android\bin\AndroidManifest.xml : \
-c:\xampp\htdocs\free-sofa\platforms\android\AndroidManifest.xml \
